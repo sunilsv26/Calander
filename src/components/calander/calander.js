421eae7ad682faa825f9d6d7d987492f41d3bf6a
Sunil <sunilsv26@gmail.com>
2020-11-03 15:33:58 +0530

    #4 Initialized calander comp

---
+++ src/components/calander/calander.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './calander.module.css';
+
+const calander =()=>{
+    return(
+        <div className={classes.Calander}></div>
+    )
+}
+
+export default calander;
